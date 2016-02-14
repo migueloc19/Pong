7dfdfec2601c1590a2945b72d9a32f9d02bc1e7d
Raydelto Hernandez <ray.hernandez@bluespurs.com>
2016-02-14 12:32:03 -0400

    Haciendo commit el código plantilla

---
+++ src/app.js
@@ -0,0 +1,56 @@
+
+var HelloWorldLayer = cc.Layer.extend({
+    jugador1:null,    
+    jugador2:null,    
+    pelota:null,    
+    puntuacion1:null,
+    puntuacion2:null,
+    inicializar:function(){
+        var size = cc.winSize;
+        var color = cc.color(100,100,100);
+
+        this.jugador1 =  new cc.DrawNode();
+        this.jugador1.drawRect(cc.p(0,0),cc.p(20,100),color,3);
+        this.jugador1.setPosition(size.width * 0.1,size.height / 2);
+        this.addChild(this.jugador1, 1);
+
+        this.jugador2 =  new cc.DrawNode();
+        this.jugador2.drawRect(cc.p(0,0),cc.p(20,100),color,3);
+        this.jugador2.setPosition(size.width -(size.width * 0.1),size.height / 2);
+        this.addChild(this.jugador2, 1);        
+
+        var lineaDivisoria =  new cc.DrawNode();
+        lineaDivisoria.drawSegment(cc.p(size.width/2,0),cc.p(size.width/2,size.height),3,color);
+        this.addChild(lineaDivisoria,0);
+        
+        this.pelota =  new cc.DrawNode();
+        this.pelota.drawCircle(cc.p(0,0),5,0,100,false,10,color);
+        this.pelota.setPosition(size.width / 2,size.height / 2);
+        this.addChild(this.pelota, 1);
+
+        this.puntuacion1 = new cc.LabelTTF("0","Arial",24);
+        this.puntuacion1.setPosition(size.width * 0.4, size.height - (size.height * 0.10));
+        this.addChild(this.puntuacion1,0);
+        
+        this.puntuacion2 = new cc.LabelTTF("0","Arial",24);
+        this.puntuacion2.setPosition(size.width - (size.width * 0.4), size.height - (size.height * 0.10));
+        this.addChild(this.puntuacion2,0);
+        
+    },
+    ctor:function () {
+        this._super();
+        this.inicializar();
+
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
